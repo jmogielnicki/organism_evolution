7610f04a1e5494a30ae4fd70949c4fdc510ffa31
John Mogielnicki <johnmogielnicki@Johns-MacBook-Air.local>
2022-11-23 15:00:15 -0800

    generate image from population of organisms

--- image.py
+++ image.py
@@ -1,11 +1,34 @@
-from PIL import Image
+import os
+import glob
+from PIL import Image, ImageDraw
+from population import Population
 
-def newImg(int):
-    img = Image.new('RGB', (400, 400))
-    img.putpixel((int, 60), (156, 156, 55))
-    return img
+img_width = 400
+img_height = 400
 
-for each in range(0, 60):
+input_width = 100
+input_height = 100
 
-    image = newImg(each)
-    image.save('/Users/johnmogielnicki/code/evolve/media/images/{}.png'.format(each), 'PNG')
+input_to_img_width_ratio = img_width / input_width
+input_to_img_height_ratio = img_height / input_height
+
+file_path = '/Users/johnmogielnicki/code/evolve/media/images/'
+files = glob.glob('{}*'.format(file_path))
+for file in files:
+    os.remove(file)
+
+
+img = Image.new('RGB', (img_width, img_height))
+d = ImageDraw.Draw(img)
+
+population = Population(10)
+for organism in population.members:
+    x = organism.position.get("x") * input_to_img_width_ratio
+    y = organism.position.get("y") * input_to_img_height_ratio
+    d.rectangle(
+        [x, y, x + input_to_img_width_ratio - 1, y + input_to_img_height_ratio - 1], 
+        fill="white",
+        outline=None,
+        width=0)
+img.save('{}{}.png'.format(file_path, 1), 'PNG')
+img.show()
--- population.py
+++ population.py
@@ -7,7 +7,11 @@ class Population:
     def __init__(self, quantity: int):
         self.quantity = quantity
         self.members = []
+        self.generate()
 
     def generate(self):
         for i in range(self.quantity):
-            self.members.append(Organism([random.randrange(0, 100), random.randrange(0, 100)]))
+            self.members.append(Organism({"x": random.randrange(0, 100), "y": random.randrange(0, 100)}))
+
+    def get_members(self):
+        return self.members
