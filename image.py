18d2d69e40281cc6664fd564367eee7039fdcbb5
John Mogielnicki <johnmogielnicki@Johns-MacBook-Air.local>
2022-11-27 09:02:22 -0800

    add tick number to image

--- image.py
+++ image.py
@@ -2,13 +2,13 @@ import os
 import glob
 import subprocess
 
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 from generation import Generation
 from food import Food
 from board import Board
 
 img_width = 900
-img_height = 900
+img_height = 940
 
 input_width = 100
 input_height = 100
@@ -23,7 +23,6 @@ def _get_files():
     return glob.glob('{}*'.format(file_path))
 
 def clear_images():
-    files = glob.glob('{}*'.format(file_path))
     for file in _get_files():
         os.remove(file)
 
@@ -31,6 +30,9 @@ def create_image(board: Board, tick: int):
     img = Image.new('RGB', (img_width, img_height))
     d = ImageDraw.Draw(img)
     board.draw(d, input_to_img_ratio)
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("Helvetica.ttc", 18)
+    draw.text((10, img_height - 20), 'tick: {}'.format(str(tick)), font=font)
     img.save('{}{}.png'.format(file_path, tick), 'PNG')
 
 def open_image():
